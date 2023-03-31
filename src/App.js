983a8c8f44e8a68f14a38df60116a9c37a4ce2ce
Sarang Kharche <sarangkharche@gmail.com>
2023-03-31 19:23:57 +0100

    Added graphql and storage - Image class erros fix

--- src/App.js
+++ src/App.js
@@ -2,22 +2,6 @@ import React, { useState, useEffect }
   from "react";
 import "./App.css";
 import "@aws-amplify/ui-react/styles.css";
-import { API } from "aws-amplify";
-import {
-  Button,
-  Flex,
-  Heading,
-  Text,
-  TextField,
-  View,
-  withAuthenticator,
-} from "@aws-amplify/ui-react";
-import { listNotes } from "./graphql/queries";
-import {
-  createNote as createNoteMutation,
-  deleteNote as deleteNoteMutation,
-} from "./graphql/mutations";
-
 import { API, Storage } from 'aws-amplify';
 import {
   Button,
@@ -29,6 +13,13 @@ import {
   View,
   withAuthenticator,
 } from '@aws-amplify/ui-react';
+import { listNotes } from "./graphql/queries";
+import {
+  createNote as createNoteMutation,
+  deleteNote as deleteNoteMutation,
+} from "./graphql/mutations";
+
+
 
 const App = ({ signOut }) => {
   const [notes, setNotes] = useState([]);
